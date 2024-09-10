8be1b99943c898b0519c9515e40c32fbe0bdf6b8
Yash Jain <jyash0294@gmail.com>
2024-09-10 19:15:20 +0530

    Some things

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookiePaser());
 app.use(checkForAuthenticationCookie("token"));
-app.use(express.static(path.resolve("./public")));
+app.use(express.static(__dirname + '/public/'));
 
 app.get("/", async (req, res) => {
   const allBlogs = await Blog.find({});
